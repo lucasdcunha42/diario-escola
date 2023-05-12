69a2669cce1f35125ac2221a481b92e82269fe94
Lucas Cunha <lucasdcunha42@gmail.com>
2023-05-12 10:37:55 -0300

    codigo inicial js

---
+++ javascript/tarefa.js
@@ -0,0 +1,17 @@
+var clientes = {
+{
+‘id’: 1,
+‘nome’: ‘Luis Santos Silveira’,
+‘idade’: 18
+},
+{
+‘id’: 2,
+‘nome’: ‘Ricardo Lopes Alves,
+‘idade’: 30
+},
+{
+‘id’: 3,
+‘nome’: ‘Gustavo Silva Junior’,
+‘idade’: 26
+}
+};
